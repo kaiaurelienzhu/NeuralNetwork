a45b62b5c00b68fecd05fcb38b55f0c54d163a96
Kai Zhu <kai.aurelien.zhu@gmail.com>
2020-09-12 15:55:49 +1000

    Initial commit

---
+++ NeuralNetwork/NNComponent.cs
@@ -0,0 +1,40 @@
+﻿namespace NeuralNetwork
+{
+    using System.Reflection;
+    using Grasshopper.Kernel;
+    using Rhino;
+
+    public abstract class NNComponent : GH_Component
+    {
+        // This is a base class that can be used by all the plugin's components. This can allow for better code reuse for:
+        // - Very commonly used functions (e.g. retrieving tolerances)
+        // - Shared setup tasks (e.g. plugin category; or if wrapping SolveInstance in exception tracking (e.g. Sentry))
+
+        private static string pluginCategory = "NeuralNetwork"; // NeuralNetworkTODO: The Grasshopper tab that all components sit in
+        public static readonly double DocAngleTolerance = RhinoDoc.ActiveDoc.ModelAngleToleranceRadians;
+        public static readonly double DocAbsTolerance = RhinoDoc.ActiveDoc.ModelAbsoluteTolerance;
+
+        // Pass the constructor parameters up to the main GH_Component abstract class
+        protected NNComponent(string name, string nickname, string description, string subCategory)
+            : base(name, nickname, description, pluginCategory, subCategory)
+        {
+        }
+
+        // Components must implement the method
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1313:Parameter names should begin with lower-case letter", Justification = "<Pending>")]
+        protected abstract void NeuralNetworkSolveInstance(IGH_DataAccess da);
+
+        // Override the main solve instance method. This allows it to be wrapped in a try/catch for error reporting purposes
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            NeuralNetworkSolveInstance(DA);
+        }
+
+        // This is provided to all plugins so it can be passed along to error reporting
+        public static string GetPluginVersion()
+        {
+            var v = Assembly.GetExecutingAssembly().GetName().Version;
+            return v.Major.ToString() + '.' + v.Minor.ToString() + '.' + v.Build.ToString();
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetworkInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace NeuralNetwork
+{
+    using System;
+    using System.Drawing;
+    using Grasshopper.Kernel;
+    using NeuralNetwork.Properties;
+
+    public class NeuralNetworkInfo : GH_AssemblyInfo
+    {
+        public override string Name => "NeuralNetwork";
+
+        public override Bitmap Icon => Resources.icons_icon_plugin;
+
+        // GHB note: if using the provided "About" component this text is output there
+        public override string Description => "NeuralNetworkTODO: Provide description of the plugin";
+
+        public override Guid Id => new Guid("69263764-3073-4c66-b093-33ad500105f1");
+
+        public override string AuthorName => "NeuralNetworkTODO: Add author name";
+
+        public override string AuthorContact => "NeuralNetworkTODO: Add author email";
+
+        // GHB note: if using the provided "About" component this URL is output there
+        public virtual Uri PluginURL => new Uri("https://github.com/philipbelesky/groundhog/");
+
+        // GHB note: if using the provided "About" component this URL is parsed there
+        public virtual Uri ReleasesFeed => new Uri("https://github.com/philipbelesky/groundhog/releases.atom");
+    }
+}
